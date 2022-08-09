8f7465d7d26dca56260cd753dd8fdf0ac39871f5
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2022-08-09 19:56:56 +0200

    Last update Di 9. Aug 19:56:55 CEST 2022

---
+++ sql/music_upd.sql
@@ -0,0 +1,132 @@
+/*insert into geo.places 
+  values 
+    (md5('Wacken'), 'Wacken');
+
+   
+insert into music.events 
+   values
+     (md5('Wacken Open Air 2022'), 'Wacken Open Air 2022', '2022.08.03', md5('Wacken'));
+*/
+
+insert into music.bands 
+  values    
+    (md5('Orca'), 'Orca', 'y'),
+    (md5('Kryn'), 'Kryn', 'm'),
+    (md5('V.I.D.A'), 'V.I.D.A', 'y'),
+    (md5('Impartial'), 'Impartial', 'm'),
+    (md5('Lycanthrope'), 'Lycanthrope', 'm'),
+    (md5('Speedemon'), 'Speedemon', 'y'),
+    (md5('The Risen Dead'), 'The Risen Dead', 'y'),
+    (md5('Divide'), 'Divide', 'y'),
+    (md5('Almøst Human'), 'Almøst Human', 'y'),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    (md5(''), '', ''),
+    
+    ;
+ 
+insert into music.bands_countries  
+  values
+    (md5('Sabaton'), md5('Sweden'));
+
+
+/*insert into music.generes 
+  values 
+    (md5(''), '')
+;*/
+--select * from music.generes g where genere like 'Melod%';
+
+insert into music.bands_generes 
+  values
+    (md5(''), md5(''));
+
+
+insert into music.bands_events 
+  values
+    (md5('Hipocrysis'), md5('Wacken Open Air 2022')),
+    (md5('Suicidal Angels'), md5('Wacken Open Air 2022')),
+    (md5('Rectal Smegma'), md5('Wacken Open Air 2022')),
+    (md5('Lost Society'), md5('Wacken Open Air 2022')),
+    (md5('Casttle Decapitation'), md5('Wacken Open Air 2022')),
+    (md5('Crypta'), md5('Wacken Open Air 2022')),
+    (md5('Blood Incarnation'), md5('Wacken Open Air 2022')),
+    (md5('Venom'), md5('Wacken Open Air 2022')),
+    (md5('Tribulation'), md5('Wacken Open Air 2022')),
+    (md5('Moonspell'), md5('Wacken Open Air 2022')),
+    (md5('Mantar'), md5('Wacken Open Air 2022')),
+    (md5('Orden Ogan'), md5('Wacken Open Air 2022')),
+    (md5('Insanity Alert'), md5('Wacken Open Air 2022')),
+    (md5('Striker'), md5('Wacken Open Air 2022')),
+    (md5('Hate'), md5('Wacken Open Air 2022')),
+    (md5('Auðn'), md5('Wacken Open Air 2022')),
+    (md5('The Spirit'), md5('Wacken Open Air 2022')),
+    (md5('Rectal Smegma'), md5('Wacken Open Air 2022')),
+    (md5('Death Angel'), md5('Wacken Open Air 2022')),
+    (md5('Implore'), md5('Wacken Open Air 2022')),
+    (md5('Ill Niño'), md5('Wacken Open Air 2022')),
+    (md5('Orca'), md5('Wacken Open Air 2022')), --
+    (md5('Kyr'), md5('Wacken Open Air 2022')), --
+    (md5('V.I.D.A'), md5('Wacken Open Air 2022')), --
+    (md5('Criminal'), md5('Wacken Open Air 2022')),
+    (md5('Onslaught'), md5('Wacken Open Air 2022')),
+    (md5('Cadaver'), md5('Wacken Open Air 2022')),
+    (md5('Divide'), md5('Wacken Open Air 2022')), --
+    (md5('Lycanthrope'), md5('Wacken Open Air 2022')), --
+    (md5('Impartial'), md5('Wacken Open Air 2022')), --
+    (md5('The Risen Dead'), md5('Wacken Open Air 2022')), --
+    (md5('Speedemon'), md5('Wacken Open Air 2022')), --
+    (md5('Mythraeum'), md5('Wacken Open Air 2022')),
+    (md5('Almost Human'), md5('Wacken Open Air 2022')),
+    (md5('Moral Putrefaction'), md5('Wacken Open Air 2022')),
+    (md5('Tranatopsy'), md5('Wacken Open Air 2022')),
+    (md5('Komodo'), md5('Wacken Open Air 2022')),
+    (md5('Muir'), md5('Wacken Open Air 2022')),
+    (md5('Gaera'), md5('Wacken Open Air 2022')),
+    (md5('Ludicia'), md5('Wacken Open Air 2022')),
+    (md5('Typhus'), md5('Wacken Open Air 2022')),
+    (md5('Fusion Bomb'), md5('Wacken Open Air 2022')),
+    (md5('Mork'), md5('Wacken Open Air 2022')),
+    (md5('Vomitory'), md5('Wacken Open Air 2022')),
+    (md5('Pestilence'), md5('Wacken Open Air 2022')),
+    (md5('Kampfar'), md5('Wacken Open Air 2022')),
+    (md5('Gwar'), md5('Wacken Open Air 2022')),
+    (md5('Belphegor'), md5('Wacken Open Air 2022')),
+    (md5('Almøst Human'), md5('Wacken Open Air 2022')) --
+   ;
+
