b530f6904e3a01c16d0684448d9d859b4e4dc431
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2023-03-03 16:20:32 +0100

    Last update Fr 3. Mär 16:20:32 CET 2023

--- sql/music_upd.sql
+++ sql/music_upd.sql
@@ -16,15 +16,15 @@ insert into music.generes
 
 insert into music.events 
    values
-     (md5(''), '', '2023.02.1', md5(' ()'))
+     (md5('Campaing for musical destruction'), 'Campaing for musical destruction', '2023.03.03', md5('Heidelberg (halle 02)'))
      ;
 
 
 
 insert into music.bands 
   values
-    (md5(''), '', 'y'),
-    (md5(''), '', 'y'),
+    (md5('Dropdead'), 'Dropdead', 'y'),
+    (md5('Escuela Grind'), 'Escuela Grind', 'y');
     (md5(''), '', 'y'),
     (md5(''), '', 'y');
     (md5(''), '', 'y'),
@@ -37,8 +37,8 @@ insert into music.bands
 
 insert into music.bands_countries 
   values
-    (md5(''), md5('')),
-    (md5(''), md5('')),
+    (md5('Dropdead'), md5('')),
+    (md5('Escuela Grind'), md5(''));
     (md5(''), md5('')),
     (md5(''), md5('')),
     (md5(''), md5('')),
@@ -47,11 +47,11 @@ insert into music.bands_countries
 
 insert into music.bands_generes 
   values
+    (md5('Dropdead'), md5('')),
     (md5(''), md5('')),
+    (md5('Escuela Grind'), md5('')),
     (md5(''), md5('')),
-    (md5(''), md5('')),
-    (md5(''), md5('')),
-    (md5(''), md5('')),
+    (md5(''), md5(''));
     (md5(''), md5('')),
     (md5(''), md5('')),
     (md5(''), md5('')),
@@ -63,10 +63,10 @@ insert into music.bands_generes
 
 insert into music.bands_events 
   values
-    (md5(''), md5('')),
-    (md5(''), md5('')),
-    (md5(''), md5('')),
-    (md5(''), md5('')),
+    (md5('Napalm Death'), md5('Campaing for musical destruction')),
+    (md5('Siberian Meat Grinder'), md5('Campaing for musical destruction')),
+    (md5('Dropdead'), md5('Campaing for musical destruction')),
+    (md5('Escuela Grind'), md5('Campaing for musical destruction'));
     (md5(''), md5('')),
     (md5(''), md5('')),
     (md5(''), md5('')),
