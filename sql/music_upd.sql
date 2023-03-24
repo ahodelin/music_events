f37e454348b43c850b19f9ba7148970d9a51ddb7
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2023-03-24 12:29:29 +0100

    Last update Fr 24. Mär 12:29:29 CET 2023

--- sql/music_upd.sql
+++ sql/music_upd.sql
@@ -1,16 +1,21 @@
-insert into geo.countries 
+/*i
+nsert into geo.countries 
   values 
     (md5(''), '', '');
-
+*/
    
 -- New events
-select music.insert_events('', '2023.03.', '', 0);
+select music.insert_events('Ravaging Europe 2023', '2023.03.29', 'Mannheim (7er-Club)', 0);
 
 -- Bands on events
-select music.insert_bands_on_events('', '');
+select music.insert_bands_on_events('Evil Invaders', 'Ravaging Europe 2023');
+select music.insert_bands_on_events('Mason', 'Ravaging Europe 2023');
+select music.insert_bands_on_events('Schizophrenia', 'Ravaging Europe 2023');
+select music.insert_bands_on_events('Warbringer', 'Ravaging Europe 2023');
 
 -- Bands countries
-select music.insert_bands_on_countries('', ''); 
+select music.insert_bands_on_countries('Mason', 'Australia'); 
 
 -- Bands generes
-select music.insert_bands_to_generes('', '');
+select music.insert_bands_to_generes('Mason', 'Thrash Metal');
+select music.insert_bands_to_generes('Mason', 'Groove Metal');
