fe4c020380c6b0b24d9322d866f10dd693d34679
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2023-03-24 11:54:32 +0100

    Last update Fr 24. Mär 11:54:32 CET 2023

--- sql/functions_to_insert.sql
+++ sql/functions_to_insert.sql
@@ -35,8 +35,6 @@ begin
   end if;end;
 $$ language plpgsql;
 
-select music.insert_events('test_event', now()::date, 'test_place', 0);
-
 -- Bands on events
 create or replace function music.insert_bands_on_events(
   ban varchar,
@@ -81,12 +79,6 @@ begin
 end;
 $$ language plpgsql;
 
-select music.insert_bands_on_events('test_band2', 'test_event');
-
-delete from music.bands_events where id_event = md5('test_event');
-delete from music.events where id_event = md5('test_event');
-delete from music.bands where band like 'test_band%';
-delete from geo.places where id_place = md5('test_place'); 
 
 -- Bands on contries
 create or replace function music.insert_bands_on_countries(
@@ -134,11 +126,6 @@ begin
 end;
 $$ language plpgsql;
 
-select music.insert_bands_on_countries('test_band', 'test_country');
-
-insert into geo.countries values (md5('test_country'), 'test_country', 'tc');
-delete from music.bands_countries where id_band = md5('test_band');
-delete from geo.countries where country = 'test_country';
 
 -- Bands plays generes
 create or replace function music.insert_bands_to_generes(
@@ -152,34 +139,37 @@ declare
   i_gene varchar;
 begin 
 	
-  select id_genere into i_gene 
-  from music.generes g
-  where genere = gene for update;
- 
-  if not found then    
-    insert into music.generes
-  	values (md5(gene), gene);
-  end if;
-	
-  select id_band into i_band
-  from music.bands
-  where band = ban for update ;
- 
-  if not found then
-  	insert into music.bands
-  	values (md5(ban), ban, 'y');
-  end if;
- 
   select id_genere, id_band into i_gene, i_band
   from music.bands_generes bg 
   where id_band = md5(ban) and id_genere = md5(gene) for update;
   
-  if not found then
+  if found then
+    return 'This combination of band and genere exist';
+  else
+  
+    select id_genere into i_gene 
+    from music.generes g
+    where genere = gene for update;
+ 
+    if not found then    
+      insert into music.generes
+  	  values (md5(gene), gene);
+    end if;
+	
+    select id_band into i_band
+    from music.bands
+    where band = ban for update ;
+ 
+    if not found then
+  	  insert into music.bands
+  	  values (md5(ban), ban, 'y');
+    end if;
+  
     insert into music.bands_generes
     values(md5(ban), md5(gene));
     return 'Band - Genere added';
-  else
-    return 'This combination of band and genere exist';
-  end if;
+    
+  end if;  
 end;
 $$ language plpgsql;
+
