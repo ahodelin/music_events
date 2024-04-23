65b6cafe1936cf1c383d3d87969ebc1f28a4b979
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2024-04-23 10:23:08 +0200

    Last update Di 23. Apr 10:23:08 CEST 2024

--- sh/json_files/bandsGeneres.sh
+++ sh/json_files/bandsGeneres.sh
@@ -5,5 +5,5 @@ dbuser=`pass postgres_user`
 echo "var bandsGeneresInfo = {" > jsonBandsGeneres.js
 echo "  \"bands_generes\":[" >> jsonBandsGeneres.js
 
-psql -U $dbuser -w -d music_events -c "select row_to_json(t)|| ',' from music.bands_generes t join music.band b on b.id_band = t.id_band order by b.band;" | sed -n '/{/p' >> jsonBandsGeneres.js
+psql -U $dbuser -w -d music_events -c "select row_to_json(t)|| ',' from music.bands_generes t join music.bands b on b.id_band = t.id_band order by b.band;" | sed -n '/{/p' >> jsonBandsGeneres.js
 echo "]};" >> jsonBandsGeneres.js
