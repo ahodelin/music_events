67c80b89b8bbbb787aa596870c9fb440770088fc
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2024-04-23 10:14:42 +0200

    Last update Di 23. Apr 10:14:42 CEST 2024

--- sh/json_files/bandsCountries.sh
+++ sh/json_files/bandsCountries.sh
@@ -4,5 +4,6 @@ dbuser=`pass postgres_user`
 
 echo "var bandsCountriesInfo = {" > jsonBandsCountries.js
 echo "  \"bands_countries\":[" >> jsonBandsCountries.js
-psql -U $dbuser -w -d music_events -c "select row_to_json(t)|| ',' from music.bands_countries t;" | sed -n '/{/p' >> jsonBandsCountries.js
+# psql -U $dbuser -w -d music_events -c "select row_to_json(t)|| ',' from music.bands_countries t;" | sed -n '/{/p' >> jsonBandsCountries.js
+psql -U $dbuser -w -d music_events -c "select row_to_json(t)|| ',' from music.bands_countries t join music.bands b on t.id_band = b.id_band order by band;"  | sed -n '/{/p' >> jsonBandsCountries.js
 echo "]};" >> jsonBandsCountries.js
--- sql/music_upd.sql
+++ sql/music_upd.sql
@@ -1,37 +1,30 @@
-
+/*
 insert into geo.countries 
   values 
-    (md5('Estonia'), 'Estonia', 'ee');
+    (md5(''), '', '');
    
 insert into geo.countries_continents
-  values (md5('Estonia'), md5('Europe'));
-
+  values (md5(''), md5(''));
+*/
 
 -- 34.00 Dying Fetus
 
 -- insert into geo.places values (md5(' ()'), ' ()');
 
 -- New events
-select music.insert_events('Fintroll + Metsatöll + Suotana Tour 2024', '2024.04.22', 'Aschaffenburg (Colos-Saal)', 0);
+select music.insert_events('', '2024.04.', '', 0);
 
 update music.events 
-set price  = 34.00
+set price  = 
 ,persons = 2
-where id_event = md5 ('Fintroll + Metsatöll + Suotana Tour 2024');
+where id_event = md5 ('');
 
 
 -- Bands on events
-select music.insert_bands_on_events('Fintroll', 'Fintroll + Metsatöll + Suotana Tour 2024');
-select music.insert_bands_on_events('Metsatöll', 'Fintroll + Metsatöll + Suotana Tour 2024');
-select music.insert_bands_on_events('Suotana', 'Fintroll + Metsatöll + Suotana Tour 2024');
+select music.insert_bands_on_events('', '');
 
 -- Bands countries
-select music.insert_bands_on_countries('Fintroll', 'Finland');
-select music.insert_bands_on_countries('Metsatöll', 'Estonia');
-select music.insert_bands_on_countries('Suotana', 'Finland');
+select music.insert_bands_on_countries('', '');
 
 -- Bands generes
-select music.insert_bands_to_generes('Fintroll', 'Blackened Folk Metal');
-select music.insert_bands_to_generes('Metsatöll', 'Folk Metal');
-select music.insert_bands_to_generes('Suotana', 'Melodic Death Metal');
-select music.insert_bands_to_generes('Suotana', 'Power Metal');
+select music.insert_bands_to_generes('', '');
