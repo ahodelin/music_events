a7042ef32c456a4a68292c42916d2284d55dbe54
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2024-05-19 09:51:49 +0200

    Last update So 19. Mai 09:51:49 CEST 2024

--- sql/music_upd.sql
+++ sql/music_upd.sql
@@ -24,9 +24,68 @@ select music.insert_bands_on_countries('', '');
 
 -- Music genre
 select music.insert_bands_to_generes('', '');
+*/
 
+/*
+-- New events
+select music.insert_events('Grabbenacht Festival 2024', '2024.05.31', 'Schriesheim', 1, 49.0, 2);
+
+
+-- Bands on events
+select music.insert_bands_on_events('Bodyfarm', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Satan''s Revenge on Mankind', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Haunted Cemetery', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Begging for Incest', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Bösedeath', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Hellknife', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Fleshsphere', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Hour of Penance', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Public Grave', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Embrace your Punishment', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Undying Lust for Cadaverous Molestation (UxLxCxM)', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Zementmord', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Lunatic Dictator', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('These Days & Those Days', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('The Creatures from the Tomb', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Napoli Violenta', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('Rectal Depravity', 'Grabbenacht Festival 2024');
+select music.insert_bands_on_events('', 'Grabbenacht Festival 2024');
+
+-- Bands from countries
+select music.insert_bands_on_countries('Satan''s Revenge on Mankind', 'DEU');
+select music.insert_bands_on_countries('Haunted Cemetery', 'DEU');
+select music.insert_bands_on_countries('Hour of Penance', 'ITA');
+select music.insert_bands_on_countries('Public Grave', 'DEU');
+select music.insert_bands_on_countries('Embrace your Punishment', 'FRA');
+select music.insert_bands_on_countries('Zementmord', 'DEU');
+select music.insert_bands_on_countries('Lunatic Dictator', 'DEU');
+select music.insert_bands_on_countries('These Days & Those Days', 'CHE');
+select music.insert_bands_on_countries('Napoli Violenta', 'ITA');
+select music.insert_bands_on_countries('Rectal Depravity', 'CHE');
+
+-- Music genre
+select music.insert_bands_to_generes('Satan''s Revenge on Mankind', 'Porno Gore Grind');
+select music.insert_bands_to_generes('Haunted Cemetery', 'Death Metal');
+select music.insert_bands_to_generes('Hour of Penance', 'Technical Death Metal');
+select music.insert_bands_to_generes('Hour of Penance', 'Brutal Death Metal');
+select music.insert_bands_to_generes('Public Grave', 'Death Metal');
+select music.insert_bands_to_generes('Embrace your Punishment', 'Deathcore');
+select music.insert_bands_to_generes('Embrace your Punishment', 'Hardcore');
+select music.insert_bands_to_generes('Embrace your Punishment', 'Brutal Death Metal');
+select music.insert_bands_to_generes('Embrace your Punishment', 'Brutal Hardcore');
+select music.insert_bands_to_generes('Zementmord', 'Grindcore');
+select music.insert_bands_to_generes('Lunatic Dictator', 'Death Metal');
+select music.insert_bands_to_generes('Lunatic Dictator', 'Thrash Metal');
+select music.insert_bands_to_generes('These Days & Those Days', 'Slamming Hardcore Death');
+select music.insert_bands_to_generes('Napoli Violenta', 'Grindcore');
+select music.insert_bands_to_generes('Napoli Violenta', 'Hardcore');
+select music.insert_bands_to_generes('Rectal Depravity', 'Death Metal');
+select music.insert_bands_to_generes('Rectal Depravity', 'Goregrind');
+
+
+*/
 ------------------------------------------------------------------------------------
-
+/*
 -- New events
 select music.insert_events('Throw them in the van European summer tour 2024', '2024.06.27', 'Weinheim (Café Central)', 0, 34.0, 2);
 
