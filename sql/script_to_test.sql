1a161586380367de6138bd5fed49c1a47dce4a77
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2024-05-21 20:02:39 +0200

    Last update Di 21. Mai 20:02:39 CEST 2024

--- sql/script_to_test.sql
+++ sql/script_to_test.sql
@@ -1,5 +1,4 @@
 
-
 --CREATE OR REPLACE VIEW music.v_generes
 --AS 
 SELECT g.id_genere,
@@ -55,101 +54,7 @@ and g.genere in (
   and active
   order by "Country", "Band";
  
-copy
-(
- select * from music.v_eu_to_metalembrace
-) to '/var/lib/postgresql/Death_Black_Speed_Europe_to_Excel.csv' with delimiter E';' csv header;
- 
 
-
--- music.v_bands_to_tex source
-
-select "event"  from music.events e
-where date_part('year', date_event) = 2018; 
-
-select * from music.bands b where id_band not in (select id_band from music.v_bands vb);
-
-select 'Punk''n''Roll' into test_name;
-select * from test_name;
-drop table test_name;
-select * from test;
-drop table test;
-
-
-alter table music.bands 
-add column active boolean default true;
-
-alter table music.bands 
-add column note varchar;
-
-
-update music.bands 
-set active = false where band in ('');
-
-
-select distinct band, likes from music.bands b 
-join music.bands_generes bg 
-  on b.id_band = bg.id_band 
-where likes = 'y'
-and active
-and bg.id_genere = 'caac3244eefed8cffee878acae427e28'; -- Deathcore
-
-select * from music.bands b where band = 'Knife';
-
-
-
-copy (select row_to_json(t)|| ',' from music.bands_countries t) to 'testing.json';
-
-select * from music.v_eu_metalembrace_to_tex 
-
-/*insert into geo.countries 
-  values 
-    (md5(''), '', '');
-   
-insert into geo.countries_continents
-  values (md5(''), md5(''));
-*/
-
-
--- music.v_bands_to_tex source
-
-CREATE OR REPLACE VIEW music.v_bands_to_tex
-AS 
-SELECT
-        CASE
-            WHEN vb.band::text ~* 'Apey|Booze|Nordic'::text THEN regexp_replace(vb.band::text, '\&'::text, '\\&'::text)::character varying
-            WHEN vb.band::text ~* 'Auðn'::text THEN regexp_replace(vb.band::text, 'ð'::text, '\\dh '::text)::character varying
-            ELSE vb.band
-        END AS "Gruppe",
-    (' & \includegraphics[width=1cm]{../4x3/'::text || vb.flag::text) || '} & '::text AS "Land",
-    (('\includegraphics[width=1cm]{'::text || '../likes/'::text) || vb.likes::text) || '} \\ \hline'::text AS "Farbe"
-   FROM music.v_bands vb;
-   
- -- music.v_events source
-
--- music.v_events_mod source
-
-CREATE OR REPLACE VIEW music.v_events_mod
-AS SELECT date_part('year'::text, e.date_event) AS year,
-    (((
-        CASE
-            WHEN date_part('day'::text, e.date_event) < 10::double precision THEN '0'::text || date_part('day'::text, e.date_event)::text
-            ELSE date_part('day'::text, e.date_event)::text
-        END || '.'::text) ||
-        CASE
-            WHEN date_part('month'::text, e.date_event) < 10::double precision THEN '0'::text || date_part('month'::text, e.date_event)::text
-            ELSE date_part('month'::text, e.date_event)::text
-        END) || '.'::text) || date_part('year'::text, e.date_event) AS date,
-    e.event,
-    p.place,
-    count(DISTINCT be.id_band) AS bands,
-    e.duration + 1 AS days
-   FROM music.events e
-     JOIN geo.places p ON p.id_place = e.id_place
-     JOIN music.bands_events be ON be.id_event = e.id_event
-  GROUP BY e.event, p.place, e.date_event, e.id_event
-  ORDER BY e.date_event;
-  -- music.v_generes source
 
 select 
   b.band,
@@ -201,3 +106,4 @@ join music.events e
 group by cube(b.band, e."event")
 order by e."event", b.band;
 
+refresh materialized view music.mv_musical_info;
