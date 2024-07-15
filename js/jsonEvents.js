bc79387ba3e10ff62fb349ac4dff290edd2d46fb
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2024-07-15 12:41:16 +0200

    Web last update Mo 15. Jul 12:41:16 CEST 2024

--- js/jsonEvents.js
+++ js/jsonEvents.js
@@ -226,4 +226,5 @@ var eventsInfo = {
  {"id_event":"a685e40a5c47edcf3a7c9c9f38155fc8","year":2024,"date":"27.06.2024","event":"Throw them in the Van European Summer Tour 2024","place":"Weinheim (Café Central)","bands":2},
  {"id_event":"b3d8150933aa73cc2b3ba1fc39b1651c","year":2024,"date":"28.06.2024","event":"Vader - 40 Years of the Apocalypse Tour 2024","place":"Mannheim (MS Connexion Complex)","bands":3},
  {"id_event":"f4ab1c6777711952be2adceed22d7fc5","year":2024,"date":"29.06.2024","event":"The Exloited - Das Bett - 2024","place":"Frankfurt am Main (Das Bett)","bands":3},
+ {"id_event":"9fe5ff8e93ce19ca4b27d5267ad7bfb3","year":2024,"date":"11.07.2024","event":"In Flammen Open Air 2024","place":"Turgau/Entenfang","bands":38},
 ]};
