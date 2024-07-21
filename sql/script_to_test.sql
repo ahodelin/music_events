b26542387117baf07b844e19d8f6736c2112fb74
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2024-07-21 06:29:58 +0200

    Last update So 21. Jul 06:29:58 CEST 2024

--- sql/script_to_test.sql
+++ sql/script_to_test.sql
@@ -1,13 +1,13 @@
-delete from music.bands_events where id_band = md5(lower(regexp_replace('Test Band', '\s|\W', '', 'g')));
-delete from music.bands_events where id_event = md5(lower(regexp_replace('Test Event', '\s|\W', '', 'g')));
+delete from music.bands_events where id_band = md5(lower(regexp_replace('Asomwel', '\s|\W', '', 'g')));
+delete from music.bands_events where id_event = md5(lower(regexp_replace('Asomwel, Warfield', '\s|\W', '', 'g')));
 delete from music.bands_countries where id_country = 'TCC';
 delete from music.bands_countries where id_band = md5(lower(regexp_replace('Test Band', '\s|\W', '', 'g')));
 delete from music.bands_genres where id_band = md5(lower(regexp_replace('Test Band', '\s|\W', '', 'g')));
 delete from music.bands_genres where id_genre = md5(lower(regexp_replace('Test Genre', '\s|\W', '', 'g')));
 
-delete from music.events where id_event = md5(lower(regexp_replace('Test Event', '\s|\W', '', 'g')));
+delete from music.events where id_event = md5(lower(regexp_replace('Asomwel, Warfield', '\s|\W', '', 'g')));
 delete from geo.places where id_place = md5(lower(regexp_replace('Test Place', '\s|\W', '', 'g')));
-delete from music.bands where id_band = md5(lower(regexp_replace('Test Band', '\s|\W', '', 'g')));
+delete from music.bands where id_band = md5(lower(regexp_replace('Asomwel', '\s|\W', '', 'g')));
 delete from music.genres where id_genre = md5(lower(regexp_replace('Test Genre', '\s|\W', '', 'g')));
 delete from geo.countries where country = 'Test Country';
 
