6488d12d85ef8a5aa1e6c2e4ec05dea6082f597b
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2024-11-01 09:05:51 +0100

    Web last update Fr 1. Nov 09:05:51 CET 2024

--- js/jsonCountries.js
+++ js/jsonCountries.js
@@ -1,7 +1,7 @@
 var countriesInfo = {
   "countries":[
  {"id_country":"DEU                             ","country":"Germany","flag":"de","bands":422},
- {"id_country":"USA                             ","country":"United States of America","flag":"us","bands":113},
+ {"id_country":"USA                             ","country":"United States of America","flag":"us","bands":115},
  {"id_country":"SWE                             ","country":"Sweden","flag":"se","bands":48},
  {"id_country":"GBR                             ","country":"United Kingdom of Great Britain and Northern Ireland","flag":"gb","bands":44},
  {"id_country":"NLD                             ","country":"Netherlands","flag":"nl","bands":33},
