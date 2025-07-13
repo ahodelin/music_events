c008459e8217a91728b9b5c8c85c9f3fc8c30dfd
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2025-07-13 16:37:35 +0200

    Web last update So 13. Jul 16:37:35 CEST 2025

--- data_js/summary_data.js
+++ data_js/summary_data.js
@@ -1,1 +1,1 @@
-const musicData_Summary = {"total_events":298,"total_bands_seen":1103,"total_money_spent":18521.27};
+const musicData_Summary = {"total_events":299,"total_bands_seen":1109,"total_money_spent":18521.27};
