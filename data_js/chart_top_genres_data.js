764e520bf2abd8aafa24787a7c8dfc86fbfcd549
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2025-11-29 08:46:38 +0100

    Web last update Sa 29. Nov 08:46:38 CET 2025

--- data_js/chart_top_genres_data.js
+++ data_js/chart_top_genres_data.js
@@ -1,1 +1,1 @@
-const musicData_ChartTopGenres = [{"genre": "Death Metal", "band_count": 317}, {"genre": "Thrash Metal", "band_count": 174}, {"genre": "Black Metal", "band_count": 170}, {"genre": "Brutal Death Metal", "band_count": 115}, {"genre": "Melodic Death Metal", "band_count": 71}];
+const musicData_ChartTopGenres = [{"genre": "Death Metal", "band_count": 317}, {"genre": "Thrash Metal", "band_count": 174}, {"genre": "Black Metal", "band_count": 171}, {"genre": "Brutal Death Metal", "band_count": 115}, {"genre": "Melodic Death Metal", "band_count": 71}];
--- data_js/summary_data.js
+++ data_js/summary_data.js
@@ -1,1 +1,1 @@
-const musicData_Summary = {"total_events":320,"total_bands_seen":1212,"total_money_spent":20577.85};
+const musicData_Summary = {"total_events":321,"total_bands_seen":1213,"total_money_spent":20645.85};
