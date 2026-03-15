1b41ad0d4dde5b74203ba1a5631217cc6b826d05
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2026-03-15 07:04:29 +0100

    Web last update So 15. Mär 07:04:29 CET 2026

--- data_js/chart_likes_data.js
+++ data_js/chart_likes_data.js
@@ -1,1 +1,1 @@
-const musicData_ChartLikes = [{"likes": "y", "band_count": 987, "preference": "Ja"}, {"likes": "m", "band_count": 186, "preference": "Neutral"}, {"likes": "n", "band_count": 60, "preference": "Nein"}];
+const musicData_ChartLikes = [{"likes": "y", "band_count": 988, "preference": "Ja"}, {"likes": "m", "band_count": 186, "preference": "Neutral"}, {"likes": "n", "band_count": 61, "preference": "Nein"}];
